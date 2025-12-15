ce6aff4b44b8934636ef081fe3db646f247b3401
parthasarathimanikandeeswaran-afk <parthasarathimanikandeeswaran@gmail.com>
2025-12-15 10:54:06 +0530

    Add files via upload

---
+++ Backend/SQLQuery1.sql
@@ -0,0 +1,6 @@
+INSERT INTO Users (UserName, Email, Password) VALUES
+('Sarathi', 'sarathi@example.com', 'sarathi@123'),
+('Priya', 'priya@example.com', 'priya@123'),
+('Karthik', 'karthik@example.com', 'karthik@123'),
+('Anjali', 'anjali@example.com', 'anjali@123'),
+('Vikram', 'vikram@example.com', 'vikram@123');
